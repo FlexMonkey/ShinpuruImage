5765e0b217abb30ea0a443e4ea60db1deb45d241
Simon Gladman <simongladman@Simons-MacBook-Pro-2.local>
2015-05-21 07:15:36 +0100

    Initial Commit

---
+++ ShinpuruImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShinpuruImage
+//
+//  Created by Simon Gladman on 21/05/2015.
+//  Copyright (c) 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
